102d42962f634a2c9a505b84ea96ccda55526798
pserb <paulserbanescu3@gmail.com>
2021-09-30 13:19:16 -0400

    added 08_hellofxn and errors.txt

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,12 @@
+public class Greet {
+	void greet(String) {
+		System.out.println(String);
+		// System.out.println("Hello there Obi Wan");
+		// System.out.println("Good day Donald, Duck");
+		// System.out.println("How it do Mr. Moo?");
+	}
+
+	public static void main(String[] args) {
+		greet("Test");
+	}
+}
