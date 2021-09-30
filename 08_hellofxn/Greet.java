ec3591e3aa6b7d76ce3b5b4a76dedcb9e1f914cd
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-09-30 17:21:22 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,12 +1,55 @@
+/* Paul Serbanescu
+APCS
+
+HW 08 - Refactor Freshie Zero -> Methods
+
+2021-09-30
+
+Discoveries:
+	void greet(String) throws an error for multiple reasons:
+		1. you must add static to void greet - java is unable to make a static refrences (main is static) to non static methods.
+		
+		2. java always wants you to specify variable types such as (String, int, boolean, double, float) - therefore greet(String) throws an error - something like greet(String s)
+			will set "s" as a String variable and fix the error
+
+	Two ways to do this:
+		static void greet(String s) {
+			System.out.println(s);
+		}
+		public static void main(String[] args) {
+			greet("your string here"); - this emulates how print() works in python
+		}
+
+		-- ALTERNATIVELY --
+
+		static void greet() {							==>	BENEFITS -- only one methond call in main which greatly declutters main and moves all repetative/not extremely vital
+		 	System.out.println("string1");						code to methods. makes it much easier to find and edit these three strings (or add however many you want)
+		 	System.out.println("string2");
+			System.out.println("string3");
+		}
+		public static void main(String[] args) {
+			greet();
+		}
+
+*/
+
 public class Greet {
-	void greet(String) {
-		System.out.println(String);
-		// System.out.println("Hello there Obi Wan");
-		// System.out.println("Good day Donald, Duck");
-		// System.out.println("How it do Mr. Moo?");
+	static void greet(String s) {
+		System.out.println(s);
+	}
+	static void alternateGreet() {
+		System.out.println("Hello there Obi-Wan Kenobi");
+		System.out.println("Good day Donald, Duck");
+		System.out.println("How it do Mr. Moo?");
 	}
 
 	public static void main(String[] args) {
-		greet("Test");
+		greet("Duck,");
+		greet("Duck...");
+		greet("GOOSE!");
+
+		System.out.println();
+
+		alternateGreet();
 	}
 }
