b5afb1de9f5452f156f479ed479202c028a1e1ae
pserb <paulserbanescu3@gmail.com>
2021-10-01 13:10:55 -0400

    added 09_bigsib

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,14 @@
+/* Paul Serbanescu
+APCS
+
+HW 09 - What Are BigSibs Good For?
+
+2021-10-01
+
+*/
+
+public class BigSib {
+	static void greet(String s) {
+		System.out.println("What it do " + s + "-do?");
+	}
+}
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,16 @@
+/* Paul Serbanescu
+APCS
+
+HW 09 - What Are BigSibs Good For?
+
+2021-10-01
+
+*/
+
+public class Greet {
+	public static void main(String[] args) {
+		BigSib.greet("Lucky");
+		BigSib.greet("Ducky");
+		BigSib.greet("Funny");
+	}
+}
