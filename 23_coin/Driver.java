8042870ef5c4c85f60d3d268f1e2c1b3e1cad1bc
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-26 00:04:19 -0400

    Update Driver.java

--- 23_coin/Driver.java
+++ 23_coin/Driver.java
@@ -37,7 +37,7 @@ public class Driver {
       wayne.flip();
       System.out.println("yours: " + yours);
       System.out.println("wayne: " + wayne);
-      /*
+
       //test equals() method
       if ( yours.equals(wayne) ) {
         System.out.println( "Matchee matchee!" );
@@ -45,7 +45,7 @@ public class Driver {
       else {
         System.out.println( "No match. Firestarter you can not be." );
       }
-      ====================BOTTOM======================*/
+      /*====================BOTTOM======================*/
 
   }//end main()
 
