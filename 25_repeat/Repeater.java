60f2b5756023e4805f64f9be0846653cd2ba42b0
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-27 22:28:57 -0400

    Update Repeater.java

--- 25_repeat/Repeater.java
+++ 25_repeat/Repeater.java
@@ -1,3 +1,19 @@
+/* Princess Peach - Paul Serbanescu, Yuki Feng, Jaylen Zeng, David Deng
+APCS
+HW25: Do I repeat myself?
+2021-10-27
+0.2
+
+DISCO:
+For recursion, using what the method returns is very useful and vital for making it work
+We use the String result which is returnedf from fenceR() in order to recursively build on |--
+
+QCC:
+N/A
+
+
+*/
+
 public class Repeater {
 
 	static String fenceW(int numPosts) {
