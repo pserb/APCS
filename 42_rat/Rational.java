b7aaae7793acd1d8d6328c2764ca6909a4312b54
pserb <paulserbanescu3@gmail.com>
2021-12-02 16:51:17 -0500

    update Rational.java

--- 42_rat/Rational.java
+++ 42_rat/Rational.java
@@ -67,54 +67,85 @@ public class Rational {
     }
 
     public int gcd() {
-      while (this.numerator != this.denominator) {
-        if (this.numerator > this.denominator) {
-          
+      int a = this.numerator;
+      int b = this.denominator;
+
+       while (a != b) {
+        if (a > b) {
+          a -= b;
+        } else {
+          b -= a;
         }
+      }
+      return a;
+    }
+
+    public void reduce() {
+      int factor = this.gcd();
+
+      this.numerator /= factor;
+      this.denominator /= factor;
+    }
+
+    public int compareTo(Rational r) {
+      Rational simple = this;
+      simple.reduce();
+      r.reduce();
+      // try to do it this way, somehow Rational being called on is changing
+      // simple.subtract(r);
+      // return simple.numerator;
+
+      // this method works
+      if ((simple.numerator == r.numerator) && (simple.denominator == r.denominator)) {
+        return 0;
+      } else if (simple.floatValue() > r.floatValue()) {
+        return 1;
+      } else {
+        return -1;
       }
     }
 
-    // GCDEW copied from HW26.
-    // public static int gcdEW(int a, int b) {
-    //   while (a != b) {
-    //     if (a > b) {
-    //       a -= b;
-    //     } else {
-    //       b -= a;
-    //     }
-    //   }
-    //   return a;
-    // }
-
 
     public static void main(String[] args) {
-      // Rational r = new Rational();
-      // Rational p = new Rational(5,3);
-      // Rational q = new Rational(1,3);
-      //
-      // System.out.println(r);
-      // System.out.println(p);
-      //
-      // System.out.println(r.floatValue());
-      // System.out.println(p.floatValue());
-      //
-      // System.out.println("\n" + q + " * " + p);
-      // q.multiply(p);
-      // System.out.println(q);
-      // System.out.println(p);
-      //
-      // System.out.println("\n" + q + " / " + p);
-      // q.divide(p);
-      // System.out.println(q);
-      // System.out.println(p);
       Rational r = new Rational(2,3); //Stores the rational number 2/3
       Rational s = new Rational(1,2); //Stores the rational number 1/2
       Rational t = new Rational(4,18); //Stores the rational number 4/18
       System.out.println(r + " + " + s);
       r.add(s);  //Adds r to s, changes r to 7/6.  s remains 1/2
-      System.out.println(r); //prints r
+      System.out.println("r: " + r); //prints r
       r.subtract(s); //subtracts s from r, changes r to 8/12 (same as 2/3)
-      System.out.println(r);
+      System.out.println("r minus s: " + r);
+      r.reduce();
+      System.out.println("r simplified: " + r);
+
+      // t is 4/18
+      System.out.println("\nGCD of t: " + t.gcd()); // should be 2
+
+      System.out.println("t: " + t);
+      t.reduce();
+      System.out.println("t simplified: " + t);
       //t.reduce(); //Changes t to 2/9
+
+      Rational big = new Rational(1,2);
+      Rational small = new Rational(1,3);
+
+      System.out.println("\nComparing 1/2 to 1/3: " + big.compareTo(small)); // should print 1
+      System.out.println("BIG N SMALL " + big + " " + small);
+      System.out.println("\nComparing 1/3 to 1/2: " + small.compareTo(big)); // should print -1
+
+
+      Rational same1 = new Rational(3,4);
+      Rational same2 = new Rational(3,4);
+
+      System.out.println("\nComparing 3/4 to 3/4: " + same1.compareTo(same2)); // should print 0
+
+
+      Rational first = new Rational(1,3);
+      Rational second = new Rational(1,2);
+
+      System.out.println(first + " " + second);
+      first.subtract(second);
+      System.out.println(first + " " + second);
+      // System.out.println(first.add(second));
     }
   }
