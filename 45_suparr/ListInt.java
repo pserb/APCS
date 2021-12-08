a4cbc691a1f7c07446175a19cbff995cffd8ee9d
pserb <paulserbanescu3@gmail.com>
2021-12-08 13:20:45 -0500

    hw45

---
+++ 45_suparr/ListInt.java
@@ -0,0 +1,6 @@
+public interface ListInt {
+  public boolean add(int x);
+  public int get(int index);
+  public int set(int index, int o);
+  public int size();
+}
---
+++ 45_suparr/SuperArray.java
@@ -0,0 +1,17 @@
+public class SuperArray implements ListInt  {
+  public boolean add(int x) {
+    return true;
+  }
+  public int get(int index) {
+    return 1;
+  }
+  public int set(int index, int o) {
+    return 1;
+  }
+  public int size() {
+    return 1;
+  }
+  public static void main(String[] args) {
+    
+  }
+}
