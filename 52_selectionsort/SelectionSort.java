6d40df1e1068b43f0d15b361c4d7c298999d6dc6
pserb <paulserbanescu3@gmail.com>
2022-01-04 14:32:30 -0500

    method working, need header

--- 52_selectionsort/SelectionSort.java
+++ 52_selectionsort/SelectionSort.java
@@ -65,26 +65,24 @@ public class SelectionSort
     //note: this version places greatest value at "rightmost" end
 
     //maxPos will point to position of SELECTION (greatest value)
-    int maxPos=0;
-    for(int i = 0; i < data.size();i++) {
-      if (data.get(i).compareTo(data.get(maxPos)) > 0) {
-        maxPos = i;
-      }
-    }
-    Comparable dummy = data.get(maxPos);
-    data.set(maxPos, data.get(data.size()-1));
-    data.set(data.size()-1,dummy);
-    for(int pass = data.size()-2; pass > -1; pass-- ) {
+    int maxPos;
+    // start at the rightmost end
+    for(int pass = data.size()-1; pass > 0; pass--) {
+      Comparable hi = data.get(0);
       System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
 
-      
-      for( int i = 0; i < pass; i ++ ) {
+      // search for biggest number to the right of index pass
+      for(maxPos = pass; maxPos > 0; maxPos--) {
         System.out.println( "maxPos: " + maxPos );//diag
         System.out.println( data );//diag
-        if ()
-        
+        if (data.get(maxPos).compareTo(hi) > 0) {
+          hi = data.get(maxPos);
+        }
       }
 
+      Comparable p1 = data.get(pass);
+      data.set(pass, hi);
+      data.set(data.indexOf(hi), p1);
       
       System.out.println( "after swap: " +  data );//diag
     }
@@ -113,6 +111,7 @@ public class SelectionSort
   public static void main( String [] args )
   {
 
+    /*===============for VOID methods=============
     ArrayList glen = new ArrayList<Integer>();
     glen.add(7);
     glen.add(1);
@@ -127,10 +126,8 @@ public class SelectionSort
     System.out.println( "ArrayList coco before sorting:\n" + coco );
     selectionSortV(coco);
     System.out.println( "ArrayList coco after sorting:\n" + coco );
-    /*===============for VOID methods=============
-    ============================================*/
+      ============================================*/
 
-    /*==========for AL-returning methods==========
       ArrayList glen = new ArrayList<Integer>();
       glen.add(7);
       glen.add(1);
@@ -142,7 +139,7 @@ public class SelectionSort
       System.out.println( "sorted version of ArrayList glen:\n"
       + glenSorted );
       System.out.println( "ArrayList glen after sorting:\n" + glen );
-
+      
       ArrayList coco = populate( 10, 1, 1000 );
       System.out.println( "ArrayList coco before sorting:\n" + coco );
       ArrayList cocoSorted = selectionSort( coco );
@@ -150,6 +147,7 @@ public class SelectionSort
       + cocoSorted );
       System.out.println( "ArrayList coco after sorting:\n" + coco );
       System.out.println( coco );
+      /*==========for AL-returning methods==========
       ============================================*/
 
   }//end main
