e50fa5a9abc359595aae6bdb1c682191f4ded92b
pserb <paulserbanescu3@gmail.com>
2022-02-11 12:39:36 -0500

    delete old

--- cobo/review/Driver.java
+++
@@ -1,9 +0,0 @@
-public class Driver {
-    public static void main(String[] args) {
-        System.out.println(Review.sentimentVal("word"));
-        System.out.println(Review.sentimentVal("thing"));
-        System.out.println(Review.sentimentVal("person"));
-
-        System.out.println(Review.totalSentiment("SimpleReview.txt"));
-    }
-}
