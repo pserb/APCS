91b6867e97e2e00802dc2a9f0ad07bf4dacdb1b8
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-03-09 22:46:35 -0500

    header

--- 73_qsort/QuickSort.java
+++ 73_qsort/QuickSort.java
@@ -1,8 +1,8 @@
-//Clyde Sinclair
-//APCS pd0
+//WON ( Paul Serbanescu, Ethan Lam, Brian Wang )
+//APCS pd6
 //HW72 -- QuickSort
 //2022-03-09w
-//time spent: _h
+//time spent: .5h
 
 /***
  * class QuickSort
@@ -10,13 +10,17 @@
  *
  * 1. Summary of QuickSort algorithm:
  * QSort(arr):
+ * Calls recursive helper method Sort, which:
+ *  1. Partitions from the middle of the supplied start and end values, creating a left half of all smaller and right half lf all larger numbers
+ *  2. Calls itself on the first and second halfs around the partition, using the partition as the end of the first half and start of the second half
+ * Array will be sorted in-place in a fashion similar to mergesort
  *
  * 2a. Worst pivot choice and associated run time:
- *
+ * Next successive number, linear pivot quadratic runtime
  * 2b. Best pivot choice and associated run time:
- *
+ * Middle element, log pivot loglin runtime
  * 3. Approach to handling duplicate values in array:
- *
+ * partition checks <= instead of <
  **/
 
 public class QuickSort extends Partition
@@ -118,29 +122,29 @@ public class QuickSort extends Partition
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 
-    
+
     //get-it-up-and-running, static test case w/ dupes:
     int [] arr2 = {7,1,5,12,3,7};
     System.out.println("\narr2 init'd to: " );
     printArr(arr2);
-    
+
     qsort( arr2 );
     System.out.println("arr2 after qsort: " );
     printArr(arr2);
-    
-    
+
+
     // arrays of randomly generated ints
     int[] arrMatey = new int[20];
     for( int i = 0; i < arrMatey.length; i++ )
     arrMatey[i] = (int)( 48 * Math.random() );
-    
+
     System.out.println("\narrMatey init'd to: " );
     printArr(arrMatey);
-    
+
     shuffle(arrMatey);
     System.out.println("arrMatey post-shuffle: " );
     printArr(arrMatey);
-    
+
     qsort( arrMatey );
     System.out.println("arrMatey after sort: " );
     printArr(arrMatey);
