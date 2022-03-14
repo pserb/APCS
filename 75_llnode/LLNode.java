ea1029299a695111ef86f4c008ebc3b11c9daf1d
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-03-14 08:49:42 -0400

    using skeleton 💀

--- 75_llnode/LLNode.java
+++ 75_llnode/LLNode.java
@@ -1,30 +1,98 @@
-// WON (Brian, Paul, Ethan)
-//APCS
-//HW 75 -- Nodal Recall -- Linked Lists
-//2020--03--13
-//time spent: .3 hours
+/***
+ * class LLNode
+ * Implements a node, for use in lists and other container classes.
+ * Stores its data as a String
+ **/
 
-public class LLNode {
+public class LLNode
+{
+  //instance vars
+  private String _car;
+  private LLNode _cdr;
 
-  private Object CAR;
-  private Node CDR;
 
-  public Node() {
-
+  // constructor
+  public LLNode( String value, LLNode next )
+  {
+    _car = value;
+    _cdr = next;
   }
 
-  public Object getCAR() {
-    return CAR;
-  }
-  public Node getCDR() {
-    return CDR;
+
+  //--------------v  ACCESSORS  v--------------
+  public String getCargo()
+  {
+    return _car;
   }
 
-  public void setCAR(Object x) {
-    CAR = x;
+  public LLNode getNext()
+  {
+    return _cdr;
   }
-  public void setCDR(Node x) {
-    CDR = x;
+  //--------------^  ACCESSORS  ^--------------
+
+
+  //--------------v  MUTATORS  v--------------
+  public String setCargo( String newCargo )
+  {
+    String oldCargo = _car;
+    _car = newCargo;
+    return oldCargo;
   }
 
-}
+  public LLNode setNext( LLNode newNext )
+  {
+    LLNode oldNext = _cdr;
+    _cdr = newNext;
+    return oldNext;
+  }
+  //--------------^  MUTATORS  ^--------------
+
+
+  // override inherited toString
+  public String toString()
+  {
+    if (_cdr != null) {
+      return _car + ", " + _cdr.toString();
+    } return _car;
+  }
+
+
+  //main method for testing
+  public static void main( String[] args )
+  {
+
+    //Below is an exercise in creating a linked list...
+
+    //Create a node
+    LLNode first = new LLNode( "cat", null );
+    System.out.println(first); // see what's happening
+
+    //Create a new node after the first
+    first.setNext( new LLNode( "dog", null ) );
+    System.out.println(first); // see what's happening
+
+    //Create a third node after the second
+    first.getNext().setNext( new LLNode( "cow", null ) );
+    System.out.println(first); // see what's happening
+
+    System.out.println();
+
+    while( first != null ) {
+      System.out.println( first );
+      first = first.getNext();
+    }
+    /* A naive list traversal, has side effects.... ??
+    */
+
+    //Q: when head ptr moves to next node in list, what happens to the node it just left?
+
+    //...so better: ?
+    //
+    //
+    //
+
+  }//end main
+
+}//end class LLNode
+
