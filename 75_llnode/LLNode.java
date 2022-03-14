955200ca51e6c63473d9daa7aab33de483eb229a
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-03-14 08:16:31 -0400

    hw75

---
+++ 75_llnode/LLNode.java
@@ -0,0 +1,30 @@
+// WON (Brian, Paul, Ethan)
+//APCS
+//HW 75 -- Nodal Recall -- Linked Lists
+//2020--03--13
+//time spent: .3 hours
+
+public class LLNode {
+
+  private Object CAR;
+  private Node CDR;
+
+  public Node() {
+
+  }
+
+  public Object getCAR() {
+    return CAR;
+  }
+  public Node getCDR() {
+    return CDR;
+  }
+
+  public void setCAR(Object x) {
+    CAR = x;
+  }
+  public void setCDR(Node x) {
+    CDR = x;
+  }
+
+}
