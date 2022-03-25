12207c0525c1e75576e31956177d9bc381fbe5b2
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-03-25 14:29:57 -0400

    closet node

--- 82_llist_itr/DLLNode.java
+++ 82_llist_itr/DLLNode.java
@@ -1,125 +1,78 @@
-// Won (Brian Wang, Ethan Lam, Paul Serbanescu)
-// APCS pd6
-// HW80 -- implement a node for a linked list
-// 2022-03-14m
-// time spent:  hrs
-
-/***
+/*****************************************************
  * class DLLNode
  * Implements a node, for use in lists and other container classes.
- * Stores its data as a String
- **/
+ *****************************************************/
 
-public class DLLNode <T>
+public class DLLNode<T> {
+
+    private T _cargo;    //cargo may only be of type T
+    private DLLNode<T> _nextNode, _prevNode; //pointers to next, prev DLLNodes
+
+
+    // constructor -- initializes instance vars
+    public DLLNode( T value, DLLNode<T> prev, DLLNode<T> next ) 
 {
-  //instance vars
-  private T _cargo;
-  private DLLNode _nextNode;
-  private DLLNode _prevNode;
-
-
-  // constructor
-  public DLLNode( T value, DLLNode next )
-  {
-    _cargo = value;
-    _nextNode = next;
-  }
-
-  public DLLNode( T value, DLLNode next, DLLNode prev )
-  {
-    _cargo = value;
-    _nextNode = next;
-    _prevNode = prev;
-  }
-
-
-  //--------------v  ACCESSORS  v--------------
-  public T getCargo()
-  {
-    return _cargo;
-  }
-
-  public DLLNode getNext()
-  {
-    return _nextNode;
-  }
-
-  public DLLNode getPrev()
-  {
-    return _prevNode;
-  }
-  //--------------^  ACCESSORS  ^--------------
-
-
-  //--------------v  MUTATORS  v--------------
-  public T setCargo( T newCargo )
-  {
-    T foo = getCargo();
-    _cargo = newCargo;
-    return foo;
-  }
-
-  public DLLNode setNext( DLLNode newNext )
-  {
-    DLLNode foo = getNext();
-    _nextNode = newNext;
-    //newNext.setPrev(this);
-    return foo;
-  }
-
-  public DLLNode setPrev( DLLNode newPrev )
-  {
-    DLLNode foo = getPrev();
-    _prevNode = newPrev;
-    return foo;
-  }
-  //--------------^  MUTATORS  ^--------------
-
-
-  // override inherited toString
-  public String toString()
-  {
-    return _cargo.toString();
-  }
-
-
-  //main method for testing
-  public static void main( String[] args )
-  {
-
-    //Below is an exercise in creating a linked list...
-
-    //Create a node
-    DLLNode first = new DLLNode( "cat", null );
-
-    //Create a new node after the first
-    first.setNext( new DLLNode( "dog", null ) );
-
-    //Create a third node after the second
-    first.getNext().setNext( new DLLNode( "cow", null ) );
-    //first.getNext().setPrev(first);
-    System.out.println(first.getNext());
-    System.out.println(first.getNext().getNext());
-    System.out.println(first.getNext().getPrev());
-    /* A naive list traversal, has side effects.... ??
-       while( first != null ) {
-       System.out.println( first );
-       first = first.getNext();
-       }
-    */
-
-    //Q: when head ptr moves to next node in list, what happens to the node it just left?
-    //A: garbage collector reclaims that memory
-
-    //  so, better: (w/o moving first)
-    /*
-    DLLNode temp = first;
-    while( temp != null ) {
-      System.out.println( temp );
-      temp = temp.getNext();
+	_cargo = value;
+	_nextNode = next;
+	_prevNode = prev;
     }
-    */
 
-  }//end main
+
+    //--------------v  ACCESSORS  v--------------
+    public T getCargo() { return _cargo; }
+
+    public DLLNode<T> getNext() { return _nextNode; }
+
+    public DLLNode<T> getPrev() { return _prevNode; }
+    //--------------^  ACCESSORS  ^--------------
+
+
+    //--------------v  MUTATORS  v--------------
+    public T setCargo( T newCargo ) {
+	T foo = getCargo();
+	_cargo = newCargo;
+	return foo;
+    }
+
+    public DLLNode<T> setNext( DLLNode<T> newNext ) {
+	DLLNode<T> foo = getNext();
+	_nextNode = newNext;
+	return foo;
+    }
+
+    public DLLNode<T> setPrev( DLLNode<T> newPrev ) {
+	DLLNode<T> foo = getPrev();
+	_prevNode = newPrev;
+	return foo;
+    }
+    //--------------^  MUTATORS  ^--------------
+
+
+    // override inherited toString
+    public String toString() { return _cargo.toString(); }
+
+
+    //main method for testing
+    public static void main( String[] args ) 
+{
+	//Below is an exercise in creating a linked list...
+
+	/*********************
+	//Create a node
+	DLLNode<String> first = new DLLNode<String>( "cat", null );
+
+	//Create a new node after the first
+	first.setNext( new DLLNode<String>( "dog", null ) );
+
+	//Create a third node after the second
+	first.getNext().setNext( new DLLNode<String>( "cow", null ) );
+
+	DLLNode temp = first; 
+	while( temp != null ) {
+	    System.out.println( temp );
+	    temp = temp.getNext();
+	}
+	***********************/
+    }//end main
 
 }//end class DLLNode
