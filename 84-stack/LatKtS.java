1905781c60ec6520daeea6fdd4886e6e13017ad4
pserb <paulserbanescu3@gmail.com>
2022-03-29 12:34:40 -0400

    wip84

---
+++ 84-stack/LatKtS.java
@@ -0,0 +1,75 @@
+/***
+ * class LatKtS
+ * SKELETON
+ * driver class for Latkes
+ * Uses a stack to reverse a text string, check for sets of matching parens.
+ **/
+
+public class LatKtS
+{
+  public static final String[] OPENINGS = {"(", "[", "{"};
+  public static final String[] CLOSINGS = {")", "]", "}"};
+
+  /***
+   * precondition:  input string has length > 0
+   * postcondition: returns reversed string s
+   *                flip("stressed") -> "desserts"
+   **/
+  public static String flip( String s )
+  {
+    Latkes cake = new Latkes(10);
+    for (int i = 0; i < s.length(); i++) {
+      cake.push(s.substring(i,i+1));
+    }
+
+    String reverso = "";
+    for (int i = 0; i < s.length(); i++) {
+      reverso += cake.pop();
+    }
+    return reverso;
+  }
+
+
+  /***
+   * precondition:  s contains only the characters {,},(,),[,]
+   * postcondition: allMatched( "({}[()])" )    -> true
+   *                allMatched( "([)]" )        -> false
+   *                allMatched( "" )            -> true
+   **/
+  public static boolean allMatched( String s )
+  {
+    Latkes milleFeuille = new Latkes(10);
+    for (int i = 0; i < s.length(); i++) {
+      String letter = s.substring(i,i+1);
+      milleFeuille.push(letter);
+      System.out.println(letter);
+
+      for (int j = 0; j < CLOSINGS.length; j++) {
+        if (letter.equals(CLOSINGS[j])) { // letter is a closing paren
+          if (milleFeuille.pop().equals(OPENINGS[j])) {
+            milleFeuille.pop();
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+
+
+  //main method to test
+  public static void main( String[] args )
+  {
+    System.out.println(flip("stressed"));
+    System.out.println(allMatched( "({}[()])" )); //true
+    System.out.println(allMatched( "([)]" ) ); //false
+    System.out.println(allMatched( "(){([])}" ) ); //true
+    System.out.println(allMatched( "](){([])}" ) ); //false
+    System.out.println(allMatched( "(){([])}(" ) ); //false
+    System.out.println(allMatched( "()[[]]{{{{((([])))}}}}" ) ); //true
+    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+      ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+  }
+
+}//end class
