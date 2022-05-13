602b6bac602b7ba3ef714053ae6fee45767645ae
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-05-13 00:03:49 -0400

    updt comment

--- 98_piority/PriorityQueue.java
+++ 98_piority/PriorityQueue.java
@@ -1,5 +1,4 @@
-// Integer version of PriorityQueue
-
+// duke ap subset definition of PriorityQueue interface
 public interface PriorityQueue<T> {
     public void add(T x);
     public boolean isEmpty();
